1a2bb6baf196c3eef2d7e3588ed82373eacf2b25
skg <skaseyg@gmail.com>
2021-10-31 20:03:35 -0400

    added some psCode

--- assets/script.js
+++ assets/script.js
@@ -1,70 +1,101 @@
+// PSEUDO =======================================
+// On webpage load
+// 		Build header with high score button and timer
+// 		Show start button and code Quiz
+// On hsBbutton click
+// 		Display out of line box [build an close button]
+// 		check if local storage has scores
+// 		Yes: Load high score local storage
+// 		No: display no local high scores
+// On startbutton click - start quiz
+// Quiz functionality
+//		hide start screen
+// 		show question 1
+// 		on answer click
+// 			check which button was clicked
+// 			check if that button is true
+// 			if yes - add time and load next question
+// 			if no - remove time and load next question
+// 		redraw questions with new text buttons
+// When time runs out or when quiz number is greater than the number of questions
+// end quiz - show final time / questions left
+// do math to take final score
+// have text field to enter name
+// combine into object and save to local storage / high scores
+
 console.log("script.js loaded");
 // DEPENDENCIES =================================
+var startScreen = document.getElementById("start-screen");
 var startButton = document.getElementById("start-button");
 var timeDisplay = document.getElementById("time-display");
+var questionDiv = document.getElementById("question-div");
+
+// DATA =========================================
 const questions = [
 	{
 		question: "1 - A",
 		answers: [
-			{aText: "A", correct: true},
-			{aText: "B", correct: false},
-			{aText: "C", correct: false},
-			{aText: "D", correct: false},
-
-		]
+			{ aText: "A", correct: true },
+			{ aText: "B", correct: false },
+			{ aText: "C", correct: false },
+			{ aText: "D", correct: false },
+		],
 	},
 	{
 		question: "2 - B",
 		answers: [
-			{aText: "A", correct: false},
-			{aText: "B", correct: true},
-			{aText: "C", correct: false},
-			{aText: "D", correct: false},
-
-		]
+			{ aText: "A", correct: false },
+			{ aText: "B", correct: true },
+			{ aText: "C", correct: false },
+			{ aText: "D", correct: false },
+		],
 	},
 	{
 		question: "3 - C",
 		answers: [
-			{aText: "A", correct: false},
-			{aText: "B", correct: false},
-			{aText: "C", correct: true},
-			{aText: "D", correct: false},
-
-		]
+			{ aText: "A", correct: false },
+			{ aText: "B", correct: false },
+			{ aText: "C", correct: true },
+			{ aText: "D", correct: false },
+		],
 	},
 	{
 		question: "4 - D",
 		answers: [
-			{aText: "A", correct: false},
-			{aText: "B", correct: false},
-			{aText: "C", correct: false},
-			{aText: "D", correct: true},
-
-		]
+			{ aText: "A", correct: false },
+			{ aText: "B", correct: false },
+			{ aText: "C", correct: false },
+			{ aText: "D", correct: true },
+		],
 	},
 	{
 		question: "5 - A",
 		answers: [
-			{aText: "A", correct: true},
-			{aText: "B", correct: false},
-			{aText: "C", correct: false},
-			{aText: "D", correct: false},
-
-		]
-	}
-]
-
-
-// DATA =========================================
+			{ aText: "A", correct: true },
+			{ aText: "B", correct: false },
+			{ aText: "C", correct: false },
+			{ aText: "D", correct: false },
+		],
+	},
+];
 
 // FUNCTIONS ====================================
 // main game function
 function quizMain() {
+	quizStart();
 	countdown();
+}
+// start game
+function quizStart() {
+	startScreen.classList.add("hidden");
+	questionDiv.classList.remove("hidden");
 }
 
 // next question
+function nextQuestion() {
+}
+
+
 // end game
 // show scores
 // play again
